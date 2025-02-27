e5ac8e14aff0d30030a90e6eac60ef58be8151ff
Kel <kelabrantes.me@gmail.com>
2025-02-27 18:11:33 -0300

    validação jwt no middleware usando a biblioteca Jose

--- src/middleware.js
+++ src/middleware.js
@@ -1,19 +1,39 @@
 import { NextResponse } from "next/server";
+import { jwtVerify } from "jose";
 
-export function middleware(req){
 
-    const token = req.cookies.get('token');
+const secret = process.env.SECRET_KEY
+const SECRET = new TextEncoder().encode(secret)
+
+
+
+
+export async function middleware(req){
+
+    const token = req.cookies.get('token')?.value;
     const {pathname} = req.nextUrl
 
     if(!token && pathname.startsWith('/dashboard')){
         return NextResponse.redirect(new URL('/login', req.url))
     }
 
-    if(token && pathname.startsWith('/login')){
-        return NextResponse.redirect(new URL('/dashboard', req.url))
+    if(token){
+        const {payload} = await jwtVerify(token, SECRET)
+        const isExpired = payload.exp * 1000 < Date.now()
+
+        if(isExpired){
+            return NextResponse.redirect(new URL('/login', req.url))
+        }
+
+        if(pathname.startsWith('/login') || pathname.startsWith('/register') ){
+            return NextResponse.redirect(new URL('/dashboard', req.url))
+        }
     }
 
-    if(token && pathname.startsWith('/register')){
-        return NextResponse.redirect(new URL('/dashboard', req.url))
-    }
+
+
 }
+
+export const config = {
+    matcher: ['/dashboard/:path*',  '/login', "/register"], // Protege todas as rotas dentro de /dashboard
+};
